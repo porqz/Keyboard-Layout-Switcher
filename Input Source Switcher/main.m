b2ec0253bd7daf2c85665c29a5b37aa2606105a7
Alexander Belov <p@mesquite.ru>
2018-09-21 18:26:55 +0700

    Initial Commit

---
+++ Input Source Switcher/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Input Source Switcher
+//
+//  Created by Alexander Belov on 21.09.2018.
+//  Copyright © 2018 Alexander Belov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
