1d525456924d71cd4d7b18390c7baf1b690aab9b
Alexander Belov <p@mesquite.ru>
2018-09-21 19:03:25 +0700

    Code added

--- Input Source Switcher/main.m
+++ Input Source Switcher/main.m
@@ -1,17 +1,51 @@
-//
-//  main.m
-//  Input Source Switcher
-//
-//  Created by Alexander Belov on 21.09.2018.
-//  Copyright © 2018 Alexander Belov. All rights reserved.
-//
+/**
+ * ------------------------------------------------------------------------------
+ *  Input Source Switcher | Written by Alexander Belov | http://github.com/porqz
+ * ------------------------------------------------------------------------------
+ *
+ * Run this application:
+ *  — Without arguments to see index of current input source
+ *  — With an index to switch current input source to input source with the index
+ *
+ * If you will pass an index to the application, the application will print:
+ *  — Nothing (if application ended normally)
+ *  — -1 (if there is no input source with the index)
+ *
+ */
 
-#import <Foundation/Foundation.h>
+#import <Carbon/Carbon.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        CFArrayRef inputSourcesList = TISCreateInputSourceList(NULL, false);
+        
+        if (argc > 1) {
+            int wantedInputSourceIndex = atoi(argv[1]);
+            TISInputSourceRef wantedInputSource = CFArrayGetValueAtIndex(inputSourcesList, wantedInputSourceIndex);
+            
+            if (!wantedInputSource) {
+                printf("%d", -1);
+                
+                return 1;
+            }
+            
+            TISSelectInputSource(wantedInputSource);
+            
+            return 0;
+        }
+        
+        // If the application was running without arguments
+        TISInputSourceRef currentInputSource = TISCopyCurrentKeyboardInputSource();
+        CFIndex inputSourcesCount = CFArrayGetCount(inputSourcesList);
+        CFArrayRef inputSourceName = TISGetInputSourceProperty(currentInputSource, kTISPropertyLocalizedName);
+        
+        for (int i = 0; i < inputSourcesCount; i++)
+            if (TISGetInputSourceProperty(CFArrayGetValueAtIndex(inputSourcesList, i), kTISPropertyLocalizedName) == inputSourceName) {
+                printf("%d", i);
+                
+                return 0;
+            }
     }
+    
     return 0;
 }
